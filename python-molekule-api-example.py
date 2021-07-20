ef171abc8e40e132b51cc28de7bcf4977d091619
Matthew <31420840+MatthewGCampbell@users.noreply.github.com>
2021-07-19 22:42:47 -0400

    Update python-molekule-api-example.py

--- python-molekule-api-example.py
+++ python-molekule-api-example.py
@@ -8,84 +8,94 @@ from requests.models import Response
 from datetime import date
 import re
 
-
-Attempts = 0
-
+debug = True
+molekule_api_url = 'https://api.molekule.com/users/me'
 print("Enter your Molekule Credentials Below!")
+def set_headers(access_token):
+    global headers
+    headers = {
+        'authorization': access_token,
+        'x-api-version': '1.0',
+        'host': 'api.molekule.com',
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'user-agent': 'Molekule/3.1.3 (com.molekule.ios; build:1167; iOS 14.0.0) Alamofire/4.9.1',
+        'Date': date.today().strftime("%a, %d %b %Y %H:%M:%S GMT")
+    }
 
-# Cognito Credentials
-
-def auth_attempts():
-    global Attempts
-    Attempts += 1
-    return Attempts
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
-# Auth Attempts
-while Attempts < 3:
-    email = input("Enter email: ")
-    if(re.match(regex, email)):
+def check_user_email():
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    Attempts = 0
+    while True:
+        global email
+        email = input("Enter your email: ")
+        global u
+        if(re.match(regex, email)):
             print("Valid Email")
-            Attempts = 0 
+            # Cognito Credentials
+            u = Cognito('us-west-2_KqrEZKC6r', '1ec4fa3oriciupg94ugoi84kkk',username=email)
+            Attempts = 0
             break
-    else:
-        print("Invalid Email")
-        auth_attempts()
-        continue
+        elif Attempts <= 3:
+            print("Invalid Email")
+            Attempts += 1
+            continue
+        elif Attempts >= 3:
+            print("Too many attempts")
+            exit()
+def check_user_password(email):
+    global u
+    Attempts = 0
+    while True:
+        while Attempts < 3:
+            password = input("Enter your password: ")
+            try:
+                u.authenticate(password=password)
+                print("Valid Password")
+                return
+            except Exception as e:
+                print("Invalid Password")
+                print(e)
+                Attempts += 1
+                print(Attempts)
+                continue
+        if Attempts >= 3:
+            print("Too many attempts")
+            exit()
+        if Attempts >= 3:
+            print("Too many attempts")
+            exit()
 
-u = Cognito('us-west-2_KqrEZKC6r','1ec4fa3oriciupg94ugoi84kkk',
-    username=email)
+# Ask for user input and check if it is valid
+check_user_email()
+check_user_password(email)
 
-if Attempts >= 3:
-    print("Too many attempts. Exiting.")
-    exit()
 
-# Password Attempts
-while Attempts < 3:
-    password = input("Enter password: ")
-    try:
-        u.authenticate(password=password)
-        print("Authenticated")
-        Attempts = 0
-        break
-    except Exception as e:
-        print("Error: Wrong Password \n")
-        print("Please try again")
-        auth_attempts()
-        continue
-if Attempts >= 3:
-    print("Too many attempts. Exiting.")
-    exit()
+# print your molekule API authorization token (uncomment to see)
+if debug:
+    print(u.access_token)
 
-#print your molekule API authorization token (uncomment to see)
-print(u.access_token)
 
-url = 'https://api.molekule.com/users/me/devices'
-headers = {
-    'authorization': u.access_token,
-    'x-api-version': '1.0',
-    'host': 'api.molekule.com',
-    'accept': 'application/json',
-    'content-type': 'application/json',
-    'user-agent': 'Molekule/3.1.3 (com.molekule.ios; build:1167; iOS 14.0.0) Alamofire/4.9.1',
-    'Date': date.today().strftime("%a, %d %b %Y %H:%M:%S GMT")
-}
+def molekule_api_call():
+    device_info_url = molekule_api_url + '/devices'
+    set_headers(u.access_token)
+    global request
+    request = requests.get(device_info_url, headers=headers)
 
-r = requests.get(url, headers=headers)
 
-json_data = json.loads(r.text)
+molekule_api_call()
+json_data = json.loads(request.text)
 
 print(json_data)
 
-response_data = r.json()
+response_data = request.json()
 
 for element in response_data["content"]:
     serialnumber = element["serialNumber"]
     purifiername = element["subProduct"]["name"]
     device_info = [serialnumber, purifiername]
     print(device_info)
- 
+
 
 PurifierChoice = input("Which Air Purifier do you want to control?")
 words = device_info[1].split()
@@ -97,55 +107,63 @@ elif (PurifierChoice != words[0]):
     exit()
 
 
-fanSpeedUrl = 'https://api.molekule.com/users/me/devices/' +device_info[0] +'/actions' +'/set-fan-speed'
+fanSpeedUrl = 'https://api.molekule.com/users/me/devices/' + device_info[0] + '/actions' + '/set-fan-speed'
 fanSpeedBody = {
     "fanSpeed": 1
 }
-print(fanSpeedBody)
 
-PowerStatusUrl = 'https://api.molekule.com/users/me/devices/' +device_info[0] +'/actions' +'/set-power-status'
+
+PowerStatusUrl = 'https://api.molekule.com/users/me/devices/' + device_info[0] + '/actions' + '/set-power-status'
 PowerStatusBody = {
     "status": "off"
 }
-print(PowerStatusBody)
 
 
-OptionSelection = input("What Would you Like to do?")
-if (OptionSelection == "set fan speed"):
-    UserInputFanSpeed = input("Which Fan Speed do you want to set? (1-5) \n")
-if (OptionSelection == "turn off"):
-    UserInputPowerStatus = input("Turn off? (y/n) \n")
-    if (UserInputPowerStatus == "y"):
-        PowerStatusBody["status"] = "off"
-        r = requests.post(PowerStatusUrl, headers=headers, data=json.dumps(PowerStatusBody), verify=True)
-if (OptionSelection == "turn on"):
-    UserInputPowerStatus = input("Turn on? (y/n) \n")
-    if (UserInputPowerStatus == "y"):
-        PowerStatusBody["status"] = "on"
-        r = requests.post(PowerStatusUrl, headers=headers, data=json.dumps(PowerStatusBody), verify=True)
-if (OptionSelection == "exit"):
-    exit()
 
-def setfanSpeed(UserInputFanSpeed, serialnumber):
-    if UserInputFanSpeed == "1":
+def setfanSpeed(InputFanSpeed):
+    global fanSpeedBody
+    if InputFanSpeed == "1":
         fanSpeedBody["fanSpeed"] = 1
         print(fanSpeedBody)
-    elif UserInputFanSpeed == "2":
+    elif InputFanSpeed == "2":
         fanSpeedBody["fanSpeed"] = 2
         print(fanSpeedBody)
-    elif UserInputFanSpeed == "3":
+    elif InputFanSpeed == "3":
         fanSpeedBody["fanSpeed"] = 3
         print(fanSpeedBody)
-    elif UserInputFanSpeed == "4":
+    elif InputFanSpeed == "4":
         fanSpeedBody["fanSpeed"] = 4
         print(fanSpeedBody)
-    elif UserInputFanSpeed == "5":
+    elif InputFanSpeed == "5":
         fanSpeedBody["fanSpeed"] = 5
         print(fanSpeedBody)
-    elif UserInputFanSpeed == "No":
+    elif InputFanSpeed == "No":
         print("OK, no change")
-    elif UserInputFanSpeed <= "0":
+    elif InputFanSpeed <= "0":
         print("No Such Value")
-    elif UserInputFanSpeed < "5":
+    elif InputFanSpeed < "5":
         print("No Such Value")
-    r = requests.post(fanSpeedUrl, headers=headers, data=json.dumps(fanSpeedBody), verify=True)
+
+while True:
+    OptionSelection = input("What Would you Like to do?")
+    if (OptionSelection == "set fan speed"):
+        UserInputFanSpeed = input("Which Fan Speed do you want to set? (1-5) \n")
+        print(serialnumber)
+        setfanSpeed(UserInputFanSpeed)
+        r = requests.post(fanSpeedUrl, headers=headers,
+            data=json.dumps(fanSpeedBody), verify=True)
+    if (OptionSelection == "turn off"):
+        UserInputPowerStatus = input("Turn off? (y/n) \n")
+        if (UserInputPowerStatus == "y"):
+            PowerStatusBody["status"] = "off"
+            r = requests.post(PowerStatusUrl, headers=headers,
+                            data=json.dumps(PowerStatusBody), verify=True)
+    if (OptionSelection == "turn on"):
+        UserInputPowerStatus = input("Turn on? (y/n) \n")
+        if (UserInputPowerStatus == "y"):
+            PowerStatusBody["status"] = "on"
+            print(PowerStatusBody)
+            r = requests.post(PowerStatusUrl, headers=headers,
+                            data=json.dumps(PowerStatusBody), verify=True)
+    if (OptionSelection == "exit"):
+        exit()
